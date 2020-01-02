5a9dbd912253c768cc4000f4f75cfef2d1d9a8ab
Zelma Garza <zelmagarza@utexas.edu>
2020-01-01 23:16:25 -0600

    Update findBuyStockPrices.java

--- findBuyStockPrices.java
+++ findBuyStockPrices.java
@@ -6,14 +6,14 @@ class stock_prices{
     int sell = 1;
     Tuple<Integer,Integer> res = new Tuple<>(array[buy],array[sell]);
     for(int i = 0; i < array.length; i++){
-      if(array[i] <= array[buy]){
+      if(array[i] <= array[buy]){ //buy is pointer to smallest number seen so far. sell is corrected because it must happen after buy
         buy = i;
         sell = i;
       }
       if(array[i] > array[sell]){
         sell = i;
       }
-      if(array[sell] - array[buy] > res.y - res.x){
+      if(array[sell] - array[buy] > res.y - res.x){ //record most profitable pair
         res.y = array[sell];
         res.x = array[buy];
       }
